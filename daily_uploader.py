7e5ecfc2e09422aa11b1717c3a075e4bdcce6f1f
David Greenwood <himynamesdave@users.noreply.github.com>
2024-12-17 06:18:37 +0000

    Fix historic (#53)
    * add mode 'mod' and 'pub' #51
    
    * fix date parse #51
    
    * Update README.md
    
    * adding action docs
    
    ---------
    
    Co-authored-by: Fadl <chaos@efqr.dev>

--- daily_uploader.py
+++ daily_uploader.py
@@ -7,19 +7,17 @@ from cve2stix.celery import start_celery
 from cve2stix.main import main as download_bundle
 import logging
 
-
 def upload_file_to_s3(filepath, s3_path):
     logging.info("uploading to %s", s3_path)
     client = boto3.client('s3', endpoint_url=os.getenv('S3_ENDPOINT_URL'))
     with open(filepath, 'rb') as f:
         client.upload_fileobj(f, os.getenv('S3_BUCKET_NAME') or "cti-public", s3_path)
 
-
 celery_process = start_celery("cve2stix.celery")
 
 yesterday = datetime.now(timezone.utc) - timedelta(days=1)
 dstr = yesterday.strftime('%Y_%m_%d')
-s3_path = f"cve2stix-github-action-output/{yesterday.strftime('%Y-%m')}/cve-bundle-{dstr}-00_00_00-{dstr}-23_59_59.json"
+s3_path = f"/{yesterday.strftime('%Y-%m')}/cve-bundle-{dstr}-00_00_00-{dstr}-23_59_59.json"
 output_filename = "stix2_objects/cve-bundle.json"
 
 logging.info("downloading bundle for %s", dstr.replace('_', '-'))
